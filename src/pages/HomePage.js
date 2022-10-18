983f783abc2de24c8e34bf3d23e3d9d2bbc98013
frejarafn <90176537+frejarafn@users.noreply.github.com>
2022-10-18 12:31:19 +0200

    header test

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,10 @@
-
+import Header from "../components/Header/Header"
 
 export default function HomePage() {
     return (
+
         <section>
+            <Header></Header>
             <h1>halløj</h1>
         </section>
     )
