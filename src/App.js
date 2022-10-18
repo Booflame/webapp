5dfda45be6d9014e4a1108c2364812d8e1e4c180
Laura Fast <lfast033@gmail.com>
2022-10-18 10:56:54 +0200

    Tilføjet parameters component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import ProfilePage from './pages/ProfilePage';
 import Nav from './components/Nav';
+import Parameters from './components/Parameters';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path='/login' element={<LoginPage />} />
         <Route path='/profile' element={<ProfilePage />} />
       </Routes>
+      <Parameters />
     </>
   );
 }
