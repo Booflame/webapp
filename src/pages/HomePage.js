07e15978863258095e1d195ea8fd970c6f169707
Laura Fast <lfast033@gmail.com>
2022-10-18 12:00:30 +0200

    fjernet toggleswitch

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,9 @@
-import ToggleSwitch from '../components/Parameters/ToggleSwitch';
+
 
 export default function HomePage() {
     return (
         <section>
             <h1>halløj</h1>
-            <ToggleSwitch />
         </section>
     )
 }
