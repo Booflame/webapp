d372d77ecc5782774c8124c30448682fc21e34dc
Laura Fast <lfast033@gmail.com>
2022-10-19 11:43:07 +0200

    rettet navlink til profilepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,15 @@
 import Header from "../components/Header/Header";
+import BotNav from "../components/BottomNavigation/BotNav"
 
 export default function HomePage() {
     return (
+        <>
+            <section>
+                <Header></Header>
+                <h1>halløj</h1>
+            </section>
+            <BotNav />
+        </>
 
-        <section>
-            <Header></Header>
-            <h1>halløj</h1>
-        </section>
     )
 }
