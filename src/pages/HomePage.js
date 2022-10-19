904d8d3765d70bceeabe05f4cea6fb6be8c5d9a0
Laura Fast <lfast033@gmail.com>
2022-10-19 09:15:56 +0200

    tilføjet image

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,9 @@
-
+import ImageUpload from "../components/ImageUpload/ImageUpload";
 
 export default function HomePage() {
     return (
         <section>
+            <ImageUpload />
             <h1>halløj</h1>
         </section>
     )
