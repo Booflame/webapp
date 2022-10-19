1fe842d64c31fc642337ddb0f5b905579e3e7e10
Booflame <Loekken.n@gmail.com>
2022-10-19 12:06:28 +0200

    skubber til laura

--- src/components/BottomNavigation/BotNav.js
+++ src/components/BottomNavigation/BotNav.js
@@ -3,11 +3,11 @@ import './BotNav.css';
 
 export default function BotNav() {
     return (
-        <nav>
-            <NavLink to="/"><img className="icons" src="/images/home_icon.png" alt="home icon"></img></NavLink>
-            <NavLink to="/"><img className="icons" src="/images/add_icon.png" alt="add icon"></img></NavLink>
-            <NavLink to="/"><img className="icons" src="/images/save_icon.png" alt="save icon"></img></NavLink>
-            <NavLink to="/pages/ProfilePage"><img className="icons" src="/images/profile_icon.png" alt="profile icon"></img></NavLink>
+        <nav className="botnav">
+            <NavLink to="/"><img className="icons" src="/images/home_icon.png" alt="home icon" /></NavLink>
+            <NavLink to="/"><img className="icons" src="/images/add_icon.png" alt="add icon" /></NavLink>
+            <NavLink to="/"><img className="icons" src="/images/save_icon.png" alt="save icon" /></NavLink>
+            <NavLink to="/pages/ProfilePage"><img className="icons" src="/images/profile_icon.png" alt="profile icon" /></NavLink>
         </nav>
     )
 }
