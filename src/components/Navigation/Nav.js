41c15333e1067021d230cbd713c815aa845c38b8
Laura Fast <lfast033@gmail.com>
2022-10-19 10:29:48 +0200

    added icons to navigation

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -4,11 +4,10 @@ import './Nav.css';
 export default function Nav() {
     return (
         <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/test">Test</NavLink>
-
+            <NavLink to="/"><img className="icons" src="/images/home_icon.png" alt="home icon"></img></NavLink>
+            <NavLink to="/"><img className="icons" src="/images/add_icon.png" alt="add icon"></img></NavLink>
+            <NavLink to="/"><img className="icons" src="/images/save_icon.png" alt="save icon"></img></NavLink>
+            <NavLink to="/"><img className="icons" src="/images/profile_icon.png" alt="profile icon"></img></NavLink>
         </nav>
     )
 }
