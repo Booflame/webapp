4263805ad89f7fcfd00d4a008e4b0c8e841c8ce1
frejarafn <90176537+frejarafn@users.noreply.github.com>
2022-10-19 10:16:52 +0200

    logos

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import ImageUpload from "../components/ImageUpload/ImageUpload";
+import Header from "../components/Header/Header";
 
 export default function HomePage() {
     return (
 
         <section>
+            <Header></Header>
             <ImageUpload />
             <h1>halløj</h1>
         </section>
