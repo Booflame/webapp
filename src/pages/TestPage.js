87aed696d5c491b4625aa969c70612bbf652d2e9
frejarafn <90176537+frejarafn@users.noreply.github.com>
2022-10-21 10:29:56 +0200

    home section

--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,9 +1,11 @@
 import Header from "../components/Header/Header"
+import HomeSection from "../components/Homescreen/HomeSection"
 export default function TestPage() {
     return (
 
         <section>
             <Header></Header>
+            <HomeSection></HomeSection>
             <h1>Dette er en test</h1>
         </section>
     )
