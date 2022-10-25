353f5c706bf6a050881fbb818b33c713f71c106b
frejarafn <90176537+frejarafn@users.noreply.github.com>
2022-10-25 11:17:09 +0200

    home minde bla

--- src/components/BottomNavigation/BotNav.js
+++ src/components/BottomNavigation/BotNav.js
@@ -4,10 +4,10 @@ import './BotNav.css';
 export default function BotNav() {
     return (
         <nav className="botnav">
-            <NavLink to="/home"><img className="icons" src="/images/home_icon.png" alt="home icon" /></NavLink>
-            <NavLink to="/"><img className="icons" src="/images/add_icon.png" alt="add icon" /></NavLink>
-            <NavLink to="/"><img className="icons" src="/images/save_icon.png" alt="save icon" /></NavLink>
-            <NavLink to="/profile"><img className="icons" src="/images/profile_icon.png" alt="profile icon" /></NavLink>
+            <NavLink to="/home"><img className="icons" src="/images/homeicon.png" alt="home icon" /></NavLink>
+            <NavLink to="/"><img className="icons" src="/images/searchicon.png" alt="add icon" /></NavLink>
+            <NavLink to="/"><img className="icons" src="/images/saveicon.png" alt="save icon" /></NavLink>
+            <NavLink to="/profile"><img className="icons" src="/images/personicon.png" alt="profile icon" /></NavLink>
         </nav>
     )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,11 @@
 import Header from "../components/Header/Header";
 import BotNav from "../components/BottomNavigation/BotNav"
-
+import HomeSection from "../components/Homescreen/HomeSection";
 export default function HomePage() {
     return (
         <>
             <Header></Header>
+            <HomeSection></HomeSection>
             <h1>Home page</h1>
             <BotNav />
         </>
