658242c06326b241cc47838cf997c6e02ad68699
Booflame <Loekken.n@gmail.com>
2022-10-26 09:50:08 +0200

    location

--- src/components/Parameters/Parameters.js
+++ src/components/Parameters/Parameters.js
@@ -1,10 +1,12 @@
 import './Parameters.css';
 import Radio from '../Radio/Radio';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Parameter() {
 
     const [values, setValues] = useState({})
+    const navigate = useNavigate();
 
     function HandleSubmit(e) {
         e.preventDefault()
@@ -12,31 +14,38 @@ export default function Parameter() {
         sessionStorage.setItem("where", values.where)
         sessionStorage.setItem("when", values.when)
         sessionStorage.setItem("budget", values.budget)
-        window.open("/date", "_self")
+        navigate("../date", {
+            state: {
+                who: values.who,
+                where: values.where,
+                when: values.when,
+                budget: values.budget
+            }
+        })
     }
 
     return (
         <form className='form-switches' onSubmit={HandleSubmit}>
             <p>Hvem</p>
-            <div onChange={e => setValues({...values, who: e.target.nextSibling.textContent})} className="radio-group-wrapper">
+            <div onChange={e => setValues({...values, who: e.target.value})} className="radio-group-wrapper">
                 <Radio name="who1" group="who" value="Mig" />
                 <Radio name="who2" group="who" value="Partner" />
                 <Radio name="who3" group="who" value="Venner" />
             </div>
             <p>Hvor</p>
-            <div onChange={e => setValues({...values, where: e.target.nextSibling.textContent})} className="radio-group-wrapper">
+            <div onChange={e => setValues({...values, where: e.target.value})} className="radio-group-wrapper">
                 <Radio name="where1" group="where" value="Ude" />
                 <Radio name="where2" group="where" value="Hjemme" />
                 <Radio name="where3" group="where" value="Begge" />
             </div>
             <p>Hvornår</p>
-            <div onChange={e => setValues({...values, when: e.target.nextSibling.textContent})} className="radio-group-wrapper">
+            <div onChange={e => setValues({...values, when: e.target.value})} className="radio-group-wrapper">
                 <Radio name="when1" group="when" value="Dag" />
                 <Radio name="when2" group="when" value="Aften" />
                 <Radio name="when3" group="when" value="Hele dagen" />
             </div>
             <p>Budget</p>
-            <div onChange={e => setValues({...values, budget: e.target.nextSibling.textContent})} className="radio-group-wrapper">
+            <div onChange={e => setValues({...values, budget: e.target.value})} className="radio-group-wrapper">
                 <Radio name="budget1" group="budget" value="Gratis" />
                 <Radio name="budget2" group="budget" value="$" />
                 <Radio name="budget3" group="budget" value="$$" />
--- src/components/Radio/Radio.js
+++ src/components/Radio/Radio.js
@@ -4,7 +4,7 @@ export default function Radio({name, group, value}) {
 
     return (
         <div className="radio-wrapper">
-            <input id={name} type="radio" name={group} />
+            <input id={name} type="radio" name={group} value={value} />
             <label className='radio-label' htmlFor={name}>{value}</label>
         </div>
     )
