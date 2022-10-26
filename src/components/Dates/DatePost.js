99d1877d40a917733059105935aa170dddce1c5a
Booflame <Loekken.n@gmail.com>
2022-10-26 12:33:27 +0200

    :)

--- src/components/Dates/DatePost.js
+++ src/components/Dates/DatePost.js
@@ -10,12 +10,17 @@ export default function DatePost({ post }) {
     // Her er post sendt med som parameter og indholdet vises herefter
     return (
         <div className="card">
-            <article>
-                <h3>{parse(post.title.rendered)}</h3>
-            </article>
             <figure>
                 <img src={image} alt={post.title.rendered} />
             </figure>
+            <article>
+                <div>
+                    <h3>{parse(post.title.rendered)}</h3>
+                    {parse(post.excerpt.rendered)}
+                </div>
+                <img className="card-icon" src="/images/savee.svg" alt="save icon" />
+            </article>
+            
             <Link to={'/dateitem/' + post.id}></Link>
         </div>
     )
