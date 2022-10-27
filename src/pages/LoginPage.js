eac165fa400721bffc53216a7b067bb9230bdd28
frejarafn <90176537+frejarafn@users.noreply.github.com>
2022-10-27 11:13:10 +0200

    login edit

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,15 +3,22 @@ import FormInput from '../components/Input/Input';
 
 export default function LoginPage() {
     return (
-        <>
+        <section className="login">
+            <img src="/images/otterlogowebblue.png" alt="Logo" />
+
             <section className="container">
                 <h1>Login</h1>
                 <form className='login-form'>
-                    <FormInput name="email" type="email" />
+                    <FormInput name="Brugernavn" type="email" />
                     <FormInput name="password" type="password" />
+
                     <button className="form-btn">Login</button>
                 </form>
+                <div className='opretlink'>
+                    <h4>Har du ikke en bruger? Opret dig her.</h4>
+                </div>
             </section>
-        </>
+
+        </section>
     )
 }
