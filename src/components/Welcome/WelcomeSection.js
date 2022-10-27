405687569c650fc0fd3f47e15b5c3cc5a1ae0dac
Laura Fast <lfast033@gmail.com>
2022-10-27 12:05:31 +0200

    landingpage

--- src/components/Welcome/WelcomeSection.js
+++ src/components/Welcome/WelcomeSection.js
@@ -3,11 +3,7 @@ import './WelcomeSection.css'
 export default function WelcomeSection() {
     return (
         <section className='welcome'>
-            <img src="/images/otterlogowebblue.png" alt="Logo" />
-            <div className='welcomebox'>
-                <h2>Velkommen til Otter!</h2>
-                <h3>Vi hjælper dig med at komme på nye ideer, spændende aktiviteter og kreative udfordringer når du skal bruge tid med venner, din partner - eller dig selv!</h3>
-            </div>
+            <img src="/images/otterlogoweb.png" alt="Logo" />
             <NavLink to="/home" className="welcomebutton" id='wbutton'>START</NavLink>
             <NavLink to="/login" className="welcomebutton" id='wbutton2'>LOGIN</NavLink>
             <div className='opretlink'>
