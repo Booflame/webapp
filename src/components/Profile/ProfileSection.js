2705a1feabc050b3042ced6a5726d6e8fc142462
Booflame <Loekken.n@gmail.com>
2022-10-31 09:11:23 +0100

    profile fixes

--- src/components/Profile/ProfileSection.js
+++ src/components/Profile/ProfileSection.js
@@ -19,7 +19,7 @@ export default function ProfileSection() {
 
                 </div>
                 <div>
-                    <div className="container">
+                    <div className="profile-container">
                         <img src="/images/profilepic.png" alt="Avatar" className="image" ></img>
                         <div className="middle">
                             <div className="text">+</div>
@@ -46,13 +46,9 @@ export default function ProfileSection() {
                 </section>
             </section>
 
-
             <section className='achsection'>
                 <h2>Achievements</h2>
                 <h3>Mine seneste achievements</h3>
-
-
-
                 <div className='achbigcard'>
                     <div className="achcard">
                         <img src="/images/ach1.png" alt="Avatar"></img>
@@ -70,7 +66,6 @@ export default function ProfileSection() {
                         </div>
                     </div>
                     <hr></hr>
-
                     <div className="achcard">
                         <img src="/images/ach3.png" alt="Avatar"></img>
                         <div className="achtext">
@@ -82,21 +77,8 @@ export default function ProfileSection() {
                     <NavLink to="/error" className='friendsbutton' id='wbutton'>
                         Se flere
                     </NavLink>
-
-
-
-
                 </div>
-
-
-
             </section>
-
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-
         </section >
     );
 }
