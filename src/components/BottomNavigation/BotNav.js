e6483bc0c56234af61182b72892ff781010b6147
frejarafn <90176537+frejarafn@users.noreply.github.com>
2022-11-01 10:47:07 +0100

    datecard btn styling

--- src/components/BottomNavigation/BotNav.js
+++ src/components/BottomNavigation/BotNav.js
@@ -8,7 +8,7 @@ export default function BotNav() {
         <nav className="botnav">
             <NavLink to="/home"><img className="icons" src="/images/homeicon.png" alt="home icon" /></NavLink>
             <NavLink to="/search"><img className="icons" src="/images/searchicon.png" alt="add icon" /></NavLink>
-            <NavLink to="/"><img className="icons" src="/images/saveicon.png" alt="save icon" /></NavLink>
+            <NavLink to="/error"><img className="icons" src="/images/saveicon.png" alt="save icon" /></NavLink>
             <NavLink to="/profile"><img className="icons" src="/images/personicon.png" alt="profile icon" /></NavLink>
         </nav>
     )
