aaef334f99fb2afa2478989e99ba2f34b5e17549
Laura Fast <lfast033@gmail.com>
2022-11-01 09:59:36 +0100

    Små rettelser til searchpage

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,6 +1,6 @@
 import BotNav from "../components/BottomNavigation/BotNav"
 import Header from "../components/Header/Header"
-import "./search.css"
+import "../components/Search/search.css"
 import CategoryOne from "../components/Search/CategoryOne"
 import CategoryTwo from "../components/Search/CategoryTwo"
 
@@ -14,9 +14,11 @@ export default function SearchPage() {
             <Header></Header>
             <div className="searchpage-title"><h1>Kategorier</h1></div>
             <p className="searchpage-text">Udforsk Otters brede udvalg af aktiviteter, samlet i inddelte kategorier. Kig dig omkring og udforsk nye, klassiske, hyggelige og spændende aktiviteter!</p>
-            <div className="tag-links"><a href="#selvforkælelse">Selvforkælelse</a></div>
-            <div className="tag-links"><a href="#efterår">Efterår</a></div>
-            <div className="tag-links"><a href="#fest">Fest</a></div>
+            <div className="tag-container">
+                <div className="tag-links"><a href="#selvforkælelse">Selvforkælelse</a></div>
+                <div className="tag-links"><a href="#efterår">Efterår</a></div>
+                <div className="tag-links"><a href="#fest">Fest</a></div>
+            </div>
             <div className="searchpage-subtitle" id="efterår"><h2>Otters udvalgte efterårsaktiviteter</h2></div>
             <CategoryOne></CategoryOne>
             <div className="searchpage-subtitle" id="selvforkælelse"><h2>Tid til selvforkælelse</h2></div>
