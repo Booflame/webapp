631947d76e0c60a8dcc3d5c48bbc86354f64a2ac
Booflame <Loekken.n@gmail.com>
2022-11-02 23:01:08 +0100

    required radio

--- src/components/Radio/Radio.js
+++ src/components/Radio/Radio.js
@@ -6,7 +6,7 @@ export default function Radio({ id, group, value }) {
 
     return (
         <div className="radio-wrapper">
-            <input id={id} type="radio" name={group} value={value} />
+            <input id={id} type="radio" name={group} value={value} required/>
             <label className='radio-label' htmlFor={id}>{value}</label>
         </div>
     )
